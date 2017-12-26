8ba0b6256ae6d8c3e15323606b45e0ce03ae34ea
nisarg <nisargpandya09@gmail.com>
2017-12-26 04:46:32 +0000

    Updated code

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,5 @@
 import pickle
 import tensorflow as tf
-# TODO: import Keras layers you need here
 import numpy as np
 from keras.layers import Input, Flatten, Dense
 from keras.models import Model
@@ -11,8 +10,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
-#flags.DEFINE_integer('batch_size', 256, "The batch size.")
-#flags.DEFINE_integer('epochs', 50, "The number of epochs.")
+flags.DEFINE_integer('epochs', 50, "The number of epochs.")
+flags.DEFINE_integer('batch_size', 256, "The batch size.")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -46,22 +45,18 @@ def main(_):
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
 
-    # TODO: define your model and hyperparams here
-    # make sure to adjust the number of classes based on
-    # the dataset
-    # 10 for cifar10
-    # 43 for traffic
     nb_classes = len(np.unique(y_train))
-    # TODO: train your model here
+
+    # define model
     input_shape = X_train.shape[1:]
     inp = Input(shape=input_shape)
     x = Flatten()(inp)
     x = Dense(nb_classes, activation='softmax')(x)
-    model = Model(inp,x)
+    model = Model(inp, x)
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(X_train, y_train, batch_size=256, epochs=50, validation_data=(X_val, y_val), shuffle=True)
-
+    # train model
+    model.fit(X_train, y_train, FLAGS.batch_size, FLAGS.epochs, validation_data=(X_val, y_val), shuffle=True)
 
 
 # parses flags and calls the `main` function above
